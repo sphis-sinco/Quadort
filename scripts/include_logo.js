264cf8fe5b4c50ffea5782eb8c196b220b9a99cc
Viwu <sinconsistencia@gmail.com>
2025-12-10 21:19:25 -0600

    usin script files lolze

---
+++ scripts/include_logo.js
@@ -0,0 +1,7 @@
+var logo = document.createElement('img')
+
+logo.src = "https://raw.githubusercontent.com/sphis-sinco/Quadort/refs/heads/main/logo/logo_official_site.png"
+logo.width = "600"
+
+document.body.appendChild(logo)
+document.body.appendChild(document.createElement("hr"))
